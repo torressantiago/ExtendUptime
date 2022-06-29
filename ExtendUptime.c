95bd4c62c7d0e05457808284fa5c5a7775025d22
torressantiago <torres_santiago@javeriana.edu.co>
2022-06-29 23:45:48 +0200

    A first well documented push

---
+++ ExtendUptime.c
@@ -0,0 +1,62 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+
+// NOTE output pin => 13
+// NOTE interrupt input pin => 2
+
+void Timer1_config();
+void externalInterrupt_config();
+
+int main(void){
+    externalInterrupt_config();
+
+    // Configure pin 13 as output
+    DDRB |= (1 << DDB5);
+
+    while(1);
+
+    return 0;
+}
+
+/*------- Configuration Functions -------*/
+void Timer1_config(){
+  	cli(); // stop interrupts
+    // 2 Hz (16000000/((31249+1)*256))
+    OCR1A = 31249;
+    // CTC
+    TCCR1A |= (1 << WGM12);
+    // Prescaler 256
+    TCCR1B |= (1 << CS12);
+    // Output Compare Match A Interrupt Enable
+    TIMSK1 |= (1 << OCIE1A);
+  	sei(); // allow interrupts
+}
+
+void externalInterrupt_config(){
+    cli(); // stop interrupts
+    // Set the rising edge of INT0 (pin 2) to generate an interrupt request
+    EICRA |= (1 << ISC01) | (1 << ISC00);
+
+    // Enable the interrupt
+    EIMSK |= (1 << INT0);
+
+    sei(); // allow interrupts
+}
+
+/*------- Interrupts -------*/
+ISR(TIMER1_COMPA_vect){
+    // Change value of output pin to 0
+	PORTB &= (0 << PORTB5);
+
+    // Clear Output Compare Match A Interrupt
+    TIMSK1 &= (0 << OCIE1A);
+}
+
+ISR(INT0_vect){
+    // Change value of output pin to 1
+    PORTB |= (1 << PORTB5);
+
+    // Set the timer
+    Timer1_config();
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ExtendUptime
-This project implements a bare metal to extend a logic high using ATMEGA328p
+This project implements a bare metal to extend a logic high using ATMEGA328p.
+
+## Setup
+The setup uses INT0 (pin 2 in Arduino UNO), a digital port which can be set to detect a rising edge and generate an interrupt as result. This interrupt the toggles an arbitrary output pin, which is set here to PORTB5 (pin 13 in Arduino UNO). Once the state of the output pin has been toggled, a timer is set to 2 Hz. Once the timer reaches its end, it will generate an interrupt that will reset the timer to stop interrupting itself and set to low the output pin's state. A setup to test this code can be found below.
+
+![setup](Doc/HighLevelSchematic.png)
+
+You can test this setup @: https://www.tinkercad.com/things/3YCzbEGLNoQ-smashing-gogo-bruticus/editel?sharecode=QqdjqGblJZ84lZONKK-AdAIN6XaM14Qd_Lhm7XkvtmE
